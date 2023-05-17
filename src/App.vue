bdad62d195070d4c8d822df722766550c27b4175
SeventyFox <smirnova.appolinaria@gmail.com>
2023-05-17 21:54:38 +0400

    initial commint

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <TheHeader></TheHeader>
+</template>
+
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div class="header">
+    <v-icon class="icon" icon="mdi-text"></v-icon>
+    <h4>Управление компанией</h4>
+    <v-icon class="icon" icon="mdi-bell"></v-icon>
+    <v-icon class="icon" icon="mdi-cart"></v-icon>
+  </div>
+
+  <v-btn :to="{ name: 'companies' }">мои компании</v-btn>
+  <v-btn :to="{ name: 'managers' }">мои менеджеры</v-btn>
+</template>
+
+<style lang="scss">
+.header {
+  display: flex;
+}
+.icon {
+  color: $icon;
+}
+</style>
